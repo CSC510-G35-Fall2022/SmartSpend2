66c25567ead075c694687df9cd89b72c0518ab84
Sanjana <sanjucheerla@gmail.com>
2022-12-01 16:53:05 -0500

    finished refactoring

--- code/code.py
+++ code/code.py
@@ -29,6 +29,7 @@ from command.edit import edit1
 from command.delete import command_delete
 from command.limit import command_limit
 from command.limit_cat import command_limitcategory
+from command.settle import command_settle
 
 
 load_dotenv()
@@ -55,35 +56,9 @@ timestamp_format = '%b %d %Y %I:%M%p'
 limit_categories = ['daily', 'monthly', 'yearly', 'View Limits']
 delete_options = ['Delete All', 'Delete a specific record']
 
-# set of implemented commands and their description
-commands = {
-    'menu': 'Display this menu',
-    'add': 'Record/Add a new spending',
-    'display': 'Show sum of expenditure for the current day/month',
-    'history': 'Display spending history',
-    'delete': 'Clear/Erase all your records',
-    'edit': 'Edit/Change spending details',
-    'limit': 'Add daily/monthly/yearly limits for spending',
-    'search': 'Search a product and comapre prices',
-    'settle': 'Settle an expense shared with you',
-    'limitcategory': 'Set Monthly limits for each spending categories'
-}
-
 bot = telebot.TeleBot(api_token)
 
 telebot.logger.setLevel(logging.INFO)
-
-
-# #Define listener for requests by user
-# def listener(user_requests):
-# 	for req in user_requests:
-# 		if(req.content_type=='text'):
-# 			print("{} name: {} chat_id: {} message: {}".format(str(datetime.now()),str(req.chat.first_name),str(req.chat.id),str(req.text)))
-
-# 			# print("{} name: {} chat_id: {} message: {}".format(str(datetime.now()),str(req.chat.first_name),str(req.chat.id)))
-
-# bot.set_update_listener(listener)
-
 
 @bot.message_handler(commands=['start', 'menu'])
 def smart_menu(m):
@@ -100,27 +75,6 @@ def smart_search(message):
     command_select(message, bot)
 
 
-# def validate_entered_amount(amount_entered):
-#     if len(amount_entered) > 0 and len(amount_entered) <= 15:
-#         if amount_entered.isdigit:
-#             if re.match("^[0-9]*\\.?[0-9]*$", amount_entered):
-#                 amount = round(float(amount_entered), 2)
-#                 if amount > 0:
-#                     return str(amount)
-#     return 0
-
-
-# async def find_user_by_username(username):
-#     try:
-#         async with TelegramClient(api_username, api_id, api_hash) as client:
-#             await client.start()
-#             if not await client.is_user_authorized():
-#                 client.send_code_request(api_id)
-#             user = await client.get_entity(username)
-#             return user
-#     except Exception as e:
-#         print("Failed to search user, details: ", e)
-
 @bot.message_handler(commands=['history'])
 def smart_history(message):
     show_history(message, bot)
@@ -130,19 +84,6 @@ def smart_history(message):
 def smart_edit(message):
     edit1(message, bot)
 
-
-# # To send the shared users the updated expense
-# async def updating_user_with_updated_expense(message, user_name, user_bills):
-#     try:
-#         user = await find_user_by_username(user_name)
-
-#         if user == None:
-#             return
-
-#         bot.send_message(user.id, 'An expense has been modified.\n The new expense for {} on {} with value of {} was shared with you.'.format(
-#             str(user_bills['number']), str(user_bills['category']), str(user_bills['timestamp'].strftime(timestamp_format)), str(user_bills['cost'])))
-#     except Exception as e:
-#         print("Error during message send to remote user : ", e)
 
 @bot.message_handler(commands=['display'])
 def smart_display(message):
@@ -165,80 +106,8 @@ def smart_limit_cat(message):
 
 # Handling /settle command
 @bot.message_handler(commands=['settle'])
-def command_settle(message):
-    chat_id = message.chat.id
-    message = bot.send_message(
-        chat_id, "Please enter the date and time of the transaction you would like to settle in the following format, Eg: Sep 21 2022 1:33PM")
-    bot.register_next_step_handler(message, settle_up)
-
-
-def settle_up(message):
-    record = dict()
-    timestamp = datetime.strptime(message.text, timestamp_format)
-
-    try:
-        user_history = db.user_bills.find(
-            {'user_telegram_id': message.chat.id})
-
-        chat_id = message.chat.id
-        if user_history is None:
-            raise Exception("Sorry! No spending records found!")
-        spend_total_str = "Here is your spending history : \n|    DATE AND TIME   | CATEGORY | AMOUNT | SHARED WITH  |\n-----------------------------------------------------------------------\n"
-        for rec in user_history:
-            # print(str(rec['timestamp'].strftime(timestamp_format)))
-            # print(message.text)
-            # print(type(rec))
-            if (str(rec['timestamp'].strftime(timestamp_format)) == message.text):
-                chat_id = message.chat.id
-                record['_id'] = rec['_id']
-                record['timestamp'] = rec['timestamp']
-                record['cost'] = rec['cost']
-                record['number'] = rec['number']
-                record['category'] = rec['category']
-                record['shared_with'] = rec['shared_with']
-                spend_total_str += '{:20s} {:20s} {:20s} {}\n'.format(str(rec['number']), str(rec['timestamp'].strftime(timestamp_format)),  str(
-                    rec['category']),  str(rec['cost']), str(rec['shared_with'][0]) if 'shared_with' in rec.keys() else "")
-        #bot.send_message(chat_id, spend_total_str)
-        # print(record)
-
-        bot.send_message(chat_id, 'The following expenditure has been selected to settle up: $' + str(
-            record['cost']) + ' for ' + str(record['category']) + ' on ' + str(record['timestamp'].strftime(timestamp_format)))
-        bot.send_message(chat_id, 'Your share of the expense is: {}'.format(
-            record['cost']/(len(record['shared_with'])+1)))
-        choice_for_settle(message, record)
-
-    except Exception as e:
-        bot.reply_to(message, "Oops!" + str(e) +
-                     str(e.__cause__) + str(e.__context__))
-
-
-def choice_for_settle(message, record):
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.row_width = 2
-    markup.add("Yes")
-    markup.add("No")
-    bot.send_message(
-        message.chat.id, 'Do you want to settle this bill?', reply_markup=markup)
-    bot.register_next_step_handler(message, post_settle_selection, record)
-
-
-def post_settle_selection(message, record):
-    chat_id = message.chat.id
-    response = message.text
-    if response == "Yes":
-        new_cost = record['cost'] - \
-            (record['cost']/(len(record['shared_with'])+1))
-        # print(new_cost)
-        settled_user_bills = db.user_bills.find_one_and_update({"_id": record['_id']}, {
-                                                               '$set': {"cost": float(new_cost)}}, return_document=ReturnDocument.AFTER)
-        paid_with_str = "Here is new expense you settled down : \n|    DATE AND TIME   | CATEGORY | AMOUNT TO BE PAID BY OTHERS | SHARED WITH | PAID BY \n-----------------------------------------------------------------------\n"
-        paid_with_str += '{:20s} {:20s} {:20s} {:20s}  \n'.format(str(settled_user_bills['timestamp'].strftime(timestamp_format)),  str(
-            settled_user_bills['category']),  str(settled_user_bills['cost']), str(settled_user_bills['shared_with']))
-        bot.send_message(chat_id, paid_with_str)
-
-    else:
-        bot.send_message(chat_id, "You did not select any expense to settle. ")
-
+def smart_settle(message):
+    command_settle(message, bot)
 
 async def main():
     try:
